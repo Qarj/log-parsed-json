69baeefba4353f1cb847efb936aa7a4c71e0cd28
Tim Buckland <tim.buckland@gmail.com>
2022-12-30 15:54:21 +0000

    doco: plan scenarios

--- ToDo.md
+++ ToDo.md
@@ -9,7 +9,7 @@
 -   [x] change simple objects to be values
 -   [ ] cope with trailing comma in object
 -   [ ] cope with escaped quotes in strings
--   [ ] cope with concatenated strings
+-   [ ] cope with concatenated strings?
 -   [ ] accept multiple arguments - but how to pass options?
 -   [ ] handle undefined arguments
 -   [ ] log compliant json with colours
---
+++ compareJsonInJson.js
@@ -0,0 +1,49 @@
+const log = require('./index.js');
+
+let obj = {};
+
+obj = {
+    headerCount: 1,
+    savedJobs: [
+        {
+            id: '111',
+            listingGlobalId: '4d0b54b3-1111-4de2-1111-09d0ded71397',
+            title: 'Electronics Test Engineer',
+            companyName: 'Eskan Electronics Ltd',
+            companyUrl: '/jobs-at/eskan-electronics/jobs',
+            companyId: 222,
+            location: 'UB6',
+            salary: 'From £25,000 to £35,000 per annum Depending on experience',
+            datePosted: '2022-12-30T12:50:56.743Z',
+            dateExpire: '2023-01-02T14:24:18.027Z',
+            relatedjobs: [1, 2, 232, 1, true, false, 'first', { none: { [null]: 'test' } }],
+            companyLogo: '',
+            companyLogoAlt: 'Eskan Electronics Ltd',
+            specialString: 'single` quote {{ } [[ ]]]"\n" \'',
+            applyUrl: '/job/11135371/apply',
+            url: 'https://www.example.com/job/electronics-test-engineer/eskan-electronics-ltd-job111',
+            isActive: true,
+            external: false,
+        },
+    ],
+};
+obj['hasJson'] = JSON.stringify(obj);
+obj['hasMoreJson'] = JSON.stringify(obj);
+obj['hasEvenMoreJson'] = JSON.stringify(obj);
+console.log('Log object with console.log()');
+console.log(obj);
+gap();
+console.log('Log object with log()');
+log(obj);
+gap();
+console.log('Log object with JSON.stringify()');
+console.log(JSON.stringify(obj));
+gap();
+console.log('Log object with log(JSON.stringify())');
+log(JSON.stringify(obj));
+
+function gap() {
+    console.log();
+    console.log();
+    console.log();
+}
