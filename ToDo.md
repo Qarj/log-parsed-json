e01244c4b272dd38bec1f1e1880c2724c5cda359
Tim Buckland <tim.buckland@gmail.com>
2022-12-30 12:51:00 +0000

    test: automatically count the number of calls to console

--- ToDo.md
+++ ToDo.md
@@ -4,6 +4,7 @@
 
 ## Backlog
 
+-   [x] tests need to fail reliably
 -   [ ] strip ansi codes from output for some assertions
 -   [ ] accept multiple arguments
 -   [ ] handle undefined arguments
--- test/test.js
+++ test/test.js
@@ -7,7 +7,10 @@ beforeEach(() => {
     jest.resetModules();
     jest.clearAllMocks();
     try {
-        fs.unlinkSync(path.join(__dirname, './testHelper.json'));
+        fs.unlinkSync(path.join(__dirname, './testHelperCalls.json'));
+    } catch (e) {}
+    try {
+        fs.unlinkSync(path.join(__dirname, './testHelperCalls.json'));
     } catch (e) {}
 });
 
@@ -21,73 +24,71 @@ test('log function is a function', (done) => {
     done();
 });
 
-test('log function returns undefined', (done) => {
-    expect(log()).toBeUndefined();
-    done();
-});
-
 test('log function logs a string', (done) => {
     const positiveAssertions = ['abcd'];
     const negativeAssertions = [];
-    const expectedCalls = 2;
     setTestHelperArguments({ value: 'abcd' });
-    testRunner(positiveAssertions, negativeAssertions, expectedCalls, done);
+    testRunner(positiveAssertions, negativeAssertions, done);
 });
 
 test('log function logs a number', (done) => {
     const positiveAssertions = ['123'];
     const negativeAssertions = [];
-    const expectedCalls = 2;
     setTestHelperArguments({ value: 123 });
-    testRunner(positiveAssertions, negativeAssertions, expectedCalls, done);
+    testRunner(positiveAssertions, negativeAssertions, done);
 });
 
 test('log function logs a boolean', (done) => {
     const positiveAssertions = ['true'];
     const negativeAssertions = ["''", ']'];
-    const expectedCalls = 2;
     setTestHelperArguments({ value: true });
-    testRunner(positiveAssertions, negativeAssertions, expectedCalls, done);
+    testRunner(positiveAssertions, negativeAssertions, done);
 });
 
 test('log function logs an object', (done) => {
     const positiveAssertions = ["'test'", '{', '}'];
     const negativeAssertions = ["''", ']'];
-    const expectedCalls = 2;
     setTestHelperArguments({ logObject: 2 });
-    testRunner(positiveAssertions, negativeAssertions, expectedCalls, done);
+    testRunner(positiveAssertions, negativeAssertions, done);
 });
 
 test('log function logs an array alternate', (done) => {
     const positiveAssertions = ["'test'", '[', ']', '32m'];
     const negativeAssertions = ["''", '{', '}'];
-    const expectedCalls = 1;
     setTestHelperArguments({ logObject: 1 });
-    testRunner(positiveAssertions, negativeAssertions, expectedCalls, done);
+    testRunner(positiveAssertions, negativeAssertions, done);
 });
 
-const testRunner = (postiveAssertions, negativeAssertions, expectedCalls, done) => {
+const testRunner = (postiveAssertions, negativeAssertions, done) => {
     // https://nikhilvijayan.com/testing-stdout-in-node-js-jest
 
     const testAppFilePath = path.join(__dirname, './testHelper.js');
     const testApp = spawn('node', [testAppFilePath]);
 
-    testApp.stdout.on('data', (data) => {
+    testApp.stdout.on('data', async (data) => {
         const finish = () => {
             testApp.kill('SIGINT');
             done();
         };
-        let consoleData = { stdoutData: '', called: 0 };
+        let calls = { stdoutData: '', called: 0 };
         try {
-            consoleData = JSON.parse(fs.readFileSync(path.join(__dirname, './testHelper.json'), 'utf8'));
+            calls = JSON.parse(fs.readFileSync(path.join(__dirname, './testHelperCalls.json'), 'utf8'));
         } catch (e) {}
-        consoleData.stdoutData += data.toString();
-        consoleData.called++;
-        fs.writeFileSync(path.join(__dirname, './testHelper.json'), JSON.stringify(consoleData));
-        if (consoleData.called < expectedCalls) return finish();
+        calls.stdoutData += data.toString();
+        calls.called++;
+        const iAmCallNumber = calls.called;
+        fs.writeFileSync(path.join(__dirname, './testHelperCalls.json'), JSON.stringify(calls));
+        await sleep(5); // expect all console.log calls to be done
+        try {
+            calls = JSON.parse(fs.readFileSync(path.join(__dirname, './testHelperCalls.json'), 'utf8'));
+        } catch (e) {}
+        try {
+            calls = JSON.parse(fs.readFileSync(path.join(__dirname, './testHelperCalls.json'), 'utf8'));
+        } catch (e) {}
+        if (calls.called > iAmCallNumber) return finish();
 
-        for (const assertion of postiveAssertions) expect(consoleData.stdoutData).toMatch(assertion);
-        for (const assertion of negativeAssertions) expect(consoleData.stdoutData).not.toMatch(assertion);
+        for (const assertion of postiveAssertions) expect(calls.stdoutData).toMatch(assertion);
+        for (const assertion of negativeAssertions) expect(calls.stdoutData).not.toMatch(assertion);
 
         finish();
     });
@@ -95,4 +96,8 @@ const testRunner = (postiveAssertions, negativeAssertions, expectedCalls, done) 
 
 function setTestHelperArguments(args) {
     fs.writeFileSync(path.join(__dirname, './testHelperArguments.json'), JSON.stringify(args));
+}
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
