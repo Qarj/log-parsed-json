524ec231debe26d9f12ce62d2233e8c7b52734b7
Tim Buckland <tim.buckland@gmail.com>
2023-01-02 20:43:26 +0000

    feat: simpler method for installing for pipe

--- ToDo.md
+++ ToDo.md
@@ -2,6 +2,10 @@
 
 ## Priority
 
+-   pipe directly to javascript https://jonlinnell.co.uk/articles/node-stdin
+-   #!/usr/bin/env node
+    "bin": "./bin.js",
+
 ## Backlog
 
 -   [ ] cope with concatenated strings?
---
+++ pretty.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { log } = require('./index.js');
+
+let data = '';
+
+async function main() {
+    for await (const chunk of process.stdin) data += chunk;
+
+    log(data);
+}
+
+main();
